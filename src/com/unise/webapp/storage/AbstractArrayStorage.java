dd0c20b7838e21e4ab882ad998e12487909469aa
Алексей <easyfreedom44@gmail.com>
2025-07-21 22:12:14 +0300

    HW3

--- src/com/unise/webapp/storage/AbstractArrayStorage.java
+++ src/com/unise/webapp/storage/AbstractArrayStorage.java
@@ -15,6 +15,35 @@ public abstract class AbstractArrayStorage implements Storage {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size > STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
 
     public Resume get(String uuid) {
         int index = getIndex(uuid);
