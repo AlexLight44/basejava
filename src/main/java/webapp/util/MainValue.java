c6b31be84e53b4d6133fb58a40d3eb3d39329eef
Alex <easyfreedom44@gmail.com>
2025-10-04 16:53:59 +0300

    HW12

--- src/main/java/webapp/util/MainValue.java
+++ src/main/java/webapp/util/MainValue.java
@@ -1,13 +1,12 @@
 package webapp.util;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MainValue {
     public static void main(String[] args) {
         int[] values = {0, 11, 145, 2, 3, 4, 5};
-        List<Integer> list = Arrays.asList(2, 51, 41, 8, 213, 3, 20);
+        List<Integer> list = Arrays.asList(2, 51, 41, 8, 21, 3, 21);
         System.out.println(minValue(values));
         System.out.println(oddOrEven(list));
     }
@@ -18,7 +17,6 @@ public class MainValue {
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        boolean isSumEven = integers.stream().mapToInt(Integer::intValue).sum() % 2 == 0;
-        return integers.stream().filter(n -> (n % 2 == 0) != isSumEven).collect(Collectors.toList());
+        return integers.stream().collect(Collectors.partitioningBy(n -> n % 2 != 0)).get(integers.stream().mapToInt(Integer::intValue).sum() % 2 == 0);
     }
 }
